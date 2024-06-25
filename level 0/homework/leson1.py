b4f3f80b65d59c04e2e4bcd7b00e7f4eb6d2a7f5
nikaasanashvili <nikan6215@gmail.com>
2024-06-25 15:58:06 +0400

    goa

---
+++ level 0/homework/leson1.py
@@ -0,0 +1,101 @@
+from turtle import  *
+
+#we wont to paint a hous
+
+
+#stap 1: draw a square
+
+speed(50)
+
+width(7)
+
+forward (200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+left(90)
+forward(200)
+#end of square
+
+#drawing a dor
+
+left(90)
+forward(70)
+left(90)
+forward(80)
+right(90)
+forward(50)
+right(90)
+forward(80)
+
+penup()
+goto(200, 200)
+pendown()
+
+
+
+right (150)
+forward(200)
+
+left(120)
+forward(200)
+
+
+penup()
+goto(200, 150)
+
+
+right(60)
+forward(30)
+
+pendown()
+
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(40)
+left(90)
+forward(20)
+left(90)
+forward(40)
+left(90)
+forward(20)
+left(90)
+forward(20)
+left(90)
+forward(40)
+
+penup()
+
+goto(0, 150)
+left(180)
+forward(30)
+pendown()
+
+forward(30)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(40)
+right(90)
+forward(20)
+right(90)
+forward(20)
+right(90)
+forward(40)
+
+
+
+
+
+exitonclick()
---
+++ level 1/classwork/cw.py
@@ -0,0 +1,12 @@
+print(17)
+print(17.4)
+print("goa")
+print("17" + "17")
+print("goa","nika")
+
+
+
+print(int("1") + int("1"))
+print(str(17) +(17))
+
+nika="name"
